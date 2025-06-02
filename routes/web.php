44f31990f7a48e94ae320c3b66241fc0484feb17
Paulo victor Braga de almeida santos <pvbraguinha@unknownca0a2c146d48.lan>
2025-06-02 21:54:21 +0100

    Add route /clear-cache to clear Laravel cache

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -21,3 +22,10 @@ Route::get('/debug-app-key', function () {
     ]);
 });
 
+// Rota para limpar cache do Laravel via web
+Route::get('/clear-cache', function () {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('config:cache');
+    return 'Cache limpo!';
+});
